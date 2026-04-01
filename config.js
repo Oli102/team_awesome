71d64281f859a800b52d338c422917fa9b9213fe
aperez28-ui <aperez28@brophybroncos.org>
2026-04-01 13:46:08 -0700

    feat: add stateEngine and config with 6 presenter states

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
-
+const CONFIG = {
+  STATE_HOLD_FRAMES: 10,
+  EYE_CONTACT_THRESHOLD: 300,
+  POSTURE_GOOD_THRESHOLD: 0.8,
+  POSTURE_POOR_THRESHOLD: 0.5,
+  POINTS: {
+    goodPosture: 2,
+    poorPosture: -2,
+    eyeContact: 1,
+    screenGlance: -3,
+    pocketHands: -2,
+    gesticulation: 1,
+  }
+};
